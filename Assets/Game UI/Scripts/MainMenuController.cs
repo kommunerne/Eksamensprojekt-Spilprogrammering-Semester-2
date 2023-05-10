70b58dbf208c377da974b0d857e6f8341b1cc749
kommunerne <johannesdrachmann@gmail.com>
2023-05-10 17:02:04 +0200

    UI FINISHED

---
+++ Assets/Game UI/Scripts/MainMenuController.cs
@@ -0,0 +1,273 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Vairbales
+    
+    // Main Menu
+    
+        // Buttons
+        private Button _loadLoadGameMenuButton;
+        private Button _loadNewGameMenuButton;
+        private Button _loadSettingsMenuButton;
+        private Button _exitMainMenu;
+    
+        // Visual Elements
+        private VisualElement _mainMenu;
+    
+    // Load Game Menu
+    
+        // Buttons
+        private Button _returnFromLoadGameMenu;
+        private Button _loadGameButton;
+   
+        // Text Elements
+        private TextElement _loadUsername;
+        private TextElement _loadPinCode;
+        
+        // Visual Elements
+        private VisualElement _loadGameMenu;
+        
+    // New Game Menu
+    
+        // Buttons
+        private Button _returnFromNewGameMenu;
+        private Button _createNewTank;
+        
+        // Text Elements
+        private TextElement _newUsername;
+        private TextElement _newPinCode;
+        
+        // Toggles
+        private Toggle _bigGunTankToggle;
+        private Toggle _sniperTankToggle;
+        private Toggle _gunnerTankToggle;
+        private Toggle _machineTankToggle;
+        
+        // Visual Elements
+        private VisualElement _newGameMenu;
+        
+    // Settings Menu
+    
+        // Buttons
+        private Button _returnFromSettingsMenu;
+        
+        // Sliders
+        private Slider _musicSlider;
+        private Slider _effectsSlider;
+        
+        // Visual Elements
+        private VisualElement _settingsMenu;
+    
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        #region Instantiating
+        // Main Menu
+        
+            // Buttons
+            _loadLoadGameMenuButton = root.Q<Button>("goToLoadGameMenu");
+            _loadNewGameMenuButton = root.Q<Button>("goToNewGameMenu");
+            _loadSettingsMenuButton = root.Q<Button>("goToSettingsMenu");
+            _exitMainMenu = root.Q<Button>("exitMainMenu");
+            
+            // Visual Elements
+            _mainMenu = root.Q<VisualElement>("MainMenu");
+
+        // Load Game Menu
+        
+            // Buttons
+            _loadGameButton = root.Q<Button>("loadGameButton");
+            _returnFromLoadGameMenu = root.Q<Button>("returnFromLoadGameMenu");
+        
+            // Text Elements
+            _loadUsername = root.Q<TextElement>("loadUsername");
+            _loadPinCode = root.Q<TextElement>("loadPinCode");
+    
+            // Visual Elements
+            _loadGameMenu = root.Q<VisualElement>("LoadGameMenu");
+
+        // New Game Menu
+                    
+            // Buttons
+            _returnFromNewGameMenu = root.Q<Button>("returnFromNewGameMenu");
+            _createNewTank = root.Q<Button>("createNewTank");
+                
+            // Toggles
+            _bigGunTankToggle = root.Q<Toggle>("bigGunTankToggle");
+            _sniperTankToggle = root.Q<Toggle>("sniperTankToggle");
+            _gunnerTankToggle = root.Q<Toggle>("gunnerTankToggle");
+            _machineTankToggle = root.Q<Toggle>("machineTankToggle");
+    
+            // Visual Elements
+            _newGameMenu = root.Q<VisualElement>("NewGameMenu");
+                        
+        // Settings Menu
+        
+            // Buttons
+            _returnFromSettingsMenu = root.Q<Button>("returnFromSettingsMenu");
+                
+            // Sliders
+            _musicSlider = root.Q<Slider>("musicSlider");
+            _effectsSlider = root.Q<Slider>("effectsSlider");
+            
+            // Visual Elements
+            _settingsMenu = root.Q<VisualElement>("SettingsMenu");
+
+            #endregion
+    
+        
+        // On game start
+
+        _mainMenu.style.display = DisplayStyle.Flex;
+        _loadGameMenu.style.display = DisplayStyle.None;
+        _newGameMenu.style.display = DisplayStyle.None;
+        _settingsMenu.style.display = DisplayStyle.None;
+
+        // Methods called on button click
+        
+            // Main Menu
+            
+            _loadLoadGameMenuButton.clicked += LoadLoadGameMenu;
+            _loadSettingsMenuButton.clicked += LoadSettingsMenu;
+            _loadNewGameMenuButton.clicked += LoadNewGameMenu;
+            _exitMainMenu.clicked += ExitGame;
+        
+            // Load Game Menu
+
+            _loadGameButton.clicked += LoadSavedGame;
+            _returnFromLoadGameMenu.clicked += UnloadLoadGameMenu;
+            
+            // New Game Menu
+
+            _createNewTank.clicked += LoadNewGame;
+            _returnFromNewGameMenu.clicked += UnloadNewGameMenu;
+            
+            // Settings Menu
+            
+            _returnFromSettingsMenu.clicked += UnloadSettingsMenu;
+
+    }
+    void Update()
+    {
+        TogglesController();
+        GetToggle();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+    // Main Menu Methods
+    
+        private void LoadLoadGameMenu()
+        {
+            _loadGameMenu.style.display = DisplayStyle.Flex;
+        }
+        private void LoadSettingsMenu()
+        {
+            _settingsMenu.style.display = DisplayStyle.Flex;
+        }
+        private void LoadNewGameMenu()
+        {
+            _newGameMenu.style.display = DisplayStyle.Flex;
+        }
+        private void ExitGame()
+        {
+            
+        }
+    
+    // LoadGame Menu Methods
+    
+        private void LoadSavedGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        private void UnloadLoadGameMenu()
+        {
+            _loadGameMenu.style.display = DisplayStyle.None;
+        }
+
+    // New Game Menu Methods
+
+    private void LoadNewGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+        private void UnloadNewGameMenu()
+        {
+            _newGameMenu.style.display = DisplayStyle.None;
+        }
+        private void TogglesController()
+        {
+            switch (GetToggle())
+            {
+                case "bigGunTank":
+                    _sniperTankToggle.value = false;
+                    _gunnerTankToggle.value = false;
+                    _machineTankToggle.value = false;
+                    break;
+                case "sniperTank":
+                    _bigGunTankToggle.value = false;
+                    _gunnerTankToggle.value = false;
+                    _machineTankToggle.value = false;
+                    break;
+                case "gunnerTank":
+                    _bigGunTankToggle.value = false;
+                    _sniperTankToggle.value = false;
+                    _machineTankToggle.value = false;
+                    break;
+                case "machineTank":
+                    _bigGunTankToggle.value = false;
+                    _sniperTankToggle.value = false;
+                    _gunnerTankToggle.value = false;
+                    break;
+                case "noneSelected":
+                    break;
+                default:
+                    break;
+            }
+        }
+        private string GetToggle()
+        {
+            if (_bigGunTankToggle.value)
+            {
+                return "bigGunTank";
+            } 
+            else if (_sniperTankToggle.value)
+            {
+                return "sniperTank";
+            } 
+            else if (_gunnerTankToggle.value)
+            {
+                return "gunnerTank";
+            } 
+            else if (_machineTankToggle.value)
+            {
+                return "machineTank";
+            } 
+            else { return "noneSelected"; }
+        }
+        
+    // Settings Menu Methods
+    
+        private void UnloadSettingsMenu()
+        {
+            _settingsMenu.style.display = DisplayStyle.None;
+        }
+        private void MusicSlider(){}
+        private void EffectsSlider(){}
+
+    
+}
--- Assets/Game UI/Scripts/PlayerUIController.cs
+++ Assets/Game UI/Scripts/PlayerUIController.cs
@@ -9,111 +9,200 @@ using UnityEngine.UIElements;
 
 public class PlayerUIController : MonoBehaviour
 {
-    private Button _hpUpgradeButton;
-    private Button _dmgUpgradeButton;
-    private Button _firerateUpgradeButton;
-    private Button _movespeedUpgradeButton;
-    private Button _hpregenUpgradeButton;
-    private Button hideStats;
-    private Button showStats;
-    private Button deathContinue;
-
+    // Stats HUD
     
-    private VisualElement statsHidden;
-    private VisualElement statsShown;
-    private VisualElement statsHover;
-    private VisualElement healthBarContainer;
-    private VisualElement uiScreen;
-    private VisualElement deathScreen;
+        // Buttons
+        private Button _hpUpgradeButton;
+        private Button _dmgUpgradeButton;
+        private Button _firerateUpgradeButton;
+        private Button _movespeedUpgradeButton;
+        private Button _hpregenUpgradeButton;
+        private Button _hideStats;
+        private Button _showStats;
+    
+        // Visual Elements
+        private VisualElement _statsHidden;
+        private VisualElement _statsShown;
+        
+        // Progress Bars
+        private ProgressBar _hpProgressBar;
+        private ProgressBar _dmgProgressBar;
+        private ProgressBar _firerateProgressBar;
+        private ProgressBar _movespeedProgressBar;
+        private ProgressBar _hpregenProgressBar;
+        
+        // Labels
+        private Label _currentLevel;
+        private Label _statusPoints;
 
-    private ProgressBar hpProgressBar;
-    private ProgressBar dmgProgressBar;
-    private ProgressBar firerateProgressBar;
-    private ProgressBar movespeedProgressBar;
-    private ProgressBar hpregenProgressBar;
-    private ProgressBar healthBar;
-
-    private Label currentLevel;
-    private Label statusPoints;
-
-    public MiniMapTestPlayer player;
+    // Death Screen
+        
+        // Buttons
+        private Button _deathContinue;
+    
+        // VisualElements
+        private VisualElement _deathScreen;
+        
+        // Labels
+        private Label _playerScore;
+        private Label _playerLevel;
+        private Label _playerTimeAlive;
+        
+    // Player
+    
+        // Player Script
+        public MiniMapTestPlayer player;
+        
+        // Visual Elements
+        private VisualElement _healthBarContainer;
+    
+        // Progress Bars
+        private ProgressBar _healthBar;
+    
+    // Main UI
+    
+        // Visual Elements
+        private VisualElement _uiScreen;
+        
+        
+    // Esc Screen
+    
+        // Buttons
+        private Button _saveButton;
+        private Button _closeButton;
+        
+        // Visual Elements
+        private VisualElement _escScreen;
+        
+        
+    
     
     // Start is called before the first frame update
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
-        
-        // Labels
-        currentLevel = root.Q<Label>("currentLevel");
-        statusPoints = root.Q<Label>("statusPoints");
-        
-        // Buttons
-        _hpUpgradeButton = root.Q<Button>("hpButton");
-        _dmgUpgradeButton = root.Q<Button>("dmgButton");
-        _firerateUpgradeButton = root.Q<Button>("firerateButton");
-        _movespeedUpgradeButton = root.Q<Button>("movespeedButton");
-        _hpregenUpgradeButton = root.Q<Button>("hpregenButton");
-        hideStats = root.Q<Button>("hideStats");
-        showStats = root.Q<Button>("showStats");
-        deathContinue = root.Q<Button>("continueButton");
-        
 
-        // Sliders
-        hpProgressBar = root.Q<ProgressBar>("hpSlider");
-        dmgProgressBar = root.Q<ProgressBar>("dmgSlider");
-        firerateProgressBar = root.Q<ProgressBar>("firerateSlider");
-        movespeedProgressBar = root.Q<ProgressBar>("movespeedSlider");
-        hpregenProgressBar = root.Q<ProgressBar>("hpregenSlider");
-        healthBar = root.Q<ProgressBar>("healthbar");
-        
-        // Visual Element
-        statsShown = root.Q<VisualElement>("PlayerStatsShown");
-        statsHidden = root.Q<VisualElement>("PlayerStatsHidden");
-        healthBarContainer = root.Q<VisualElement>("PlayerHealthbar");
-        uiScreen = root.Q<VisualElement>("MainScreen");
-        deathScreen = root.Q<VisualElement>("DeathScreen");
-        
+        #region Instantiation
 
+        // Stats HUD
         
-        statsShown.style.display = DisplayStyle.Flex;
-        statsHidden.style.display = DisplayStyle.None;
-        uiScreen.style.display = DisplayStyle.Flex;
-        deathScreen.style.display = DisplayStyle.None;
+            // Labels
+            _currentLevel = root.Q<Label>("currentLevel");
+            _statusPoints = root.Q<Label>("statusPoints");
+        
+            // Buttons
+            _hpUpgradeButton = root.Q<Button>("hpButton");
+            _dmgUpgradeButton = root.Q<Button>("dmgButton");
+            _firerateUpgradeButton = root.Q<Button>("firerateButton");
+            _movespeedUpgradeButton = root.Q<Button>("movespeedButton");
+            _hpregenUpgradeButton = root.Q<Button>("hpregenButton");
+            _hideStats = root.Q<Button>("hideStats");
+            _showStats = root.Q<Button>("showStats");
+        
+            // Sliders
+            _hpProgressBar = root.Q<ProgressBar>("hpSlider");
+            _dmgProgressBar = root.Q<ProgressBar>("dmgSlider");
+            _firerateProgressBar = root.Q<ProgressBar>("firerateSlider");
+            _movespeedProgressBar = root.Q<ProgressBar>("movespeedSlider");
+            _hpregenProgressBar = root.Q<ProgressBar>("hpregenSlider");
+
+            // Visual Element
+            _statsShown = root.Q<VisualElement>("PlayerStatsShown");
+            _statsHidden = root.Q<VisualElement>("PlayerStatsHidden");
+            
+        // Death Screen
+        
+            // Buttons
+            _deathContinue = root.Q<Button>("continueButton");
+        
+            // Labels
+            _playerScore = root.Q<Label>("playerScore");
+            _playerLevel = root.Q<Label>("playerLevel");
+            _playerTimeAlive = root.Q<Label>("playerTimeAlive");
+            
+            // Visual Elements
+            _deathScreen = root.Q<VisualElement>("DeathScreenContainer");
+        
+        // Player
+            // Visual Elements
+            _healthBarContainer = root.Q<VisualElement>("PlayerHealthbar");
+            
+            // Progress Bars
+            _healthBar = root.Q<ProgressBar>("healthbar");
+        
+        // Main UI
+
+            // Visual Elements
+            _uiScreen = root.Q<VisualElement>("MainScreen");
+            
+        
+        // Esc Screen
+        
+            // Buttons
+            _saveButton = root.Q<Button>("saveButton");
+            _closeButton = root.Q<Button>("closeButton");
+            
+            
+            // Visual Elements
+            _escScreen = root.Q<VisualElement>("EscScreenContainer");
+            
+         
+
+            #endregion
         
         
+        // On Start Values
+        
+        _statsShown.style.display = DisplayStyle.Flex;
+        _statsHidden.style.display = DisplayStyle.None;
+        _uiScreen.style.display = DisplayStyle.Flex;
+        _deathScreen.style.display = DisplayStyle.None;
+        _escScreen.style.display = DisplayStyle.None;
 
-        hpProgressBar.value = 0f;
-        dmgProgressBar.value = 0f;
-        firerateProgressBar.value = 0f;
-        movespeedProgressBar.value = 0f;
-        hpregenProgressBar.value = 0f;
-        healthBarContainer.style.display = DisplayStyle.None;
+        _hpProgressBar.value = 0f;
+        _dmgProgressBar.value = 0f;
+        _firerateProgressBar.value = 0f;
+        _movespeedProgressBar.value = 0f;
+        _hpregenProgressBar.value = 0f;
+        _healthBarContainer.style.display = DisplayStyle.None;
 
-        hideStats.clicked += HideStats;
-        showStats.clicked += ShowStats;
-        _hpUpgradeButton.clicked += HpUpgrade;
-        _dmgUpgradeButton.clicked += DmgUpgrade;
-        _firerateUpgradeButton.clicked += FirerateUpgrade;
-        _movespeedUpgradeButton.clicked += MovespeedUpgrade;
-        _hpregenUpgradeButton.clicked += HpregenUpgrade;
-        deathContinue.clicked += ContinueToMainMenu;
+        // On Buttons Clicked
+            
+            // Stats HUD
+            _hideStats.clicked += HideStats;
+            _showStats.clicked += ShowStats;
+            _hpUpgradeButton.clicked += HpUpgrade;
+            _dmgUpgradeButton.clicked += DmgUpgrade;
+            _firerateUpgradeButton.clicked += FirerateUpgrade;
+            _movespeedUpgradeButton.clicked += MovespeedUpgrade;
+            _hpregenUpgradeButton.clicked += HpregenUpgrade;
+            
+            // Death Screen
+            _deathContinue.clicked += ContinueToMainMenu;
 
+            // Esc Screen
+            _saveButton.clicked += SaveGame;
+            _closeButton.clicked += CloseEscScreen;
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdatePlayerHealth();
-        currentLevel.text = player.level.ToString();
-        statusPoints.text = player.statPoints.ToString();
+        
+        if(Input.GetKeyDown(KeyCode.Escape))
+            ShowEscScreen();
+        
+        _currentLevel.text = player.level.ToString();
+        _statusPoints.text = player.statPoints.ToString();
         if (player.statPoints == 0)
         {
-            statsShown.style.display = DisplayStyle.None;
+            _statsShown.style.display = DisplayStyle.None;
         }
 
         if (player.playerGotHit)
         {
-            healthBarContainer.style.display = DisplayStyle.Flex;
+            _healthBarContainer.style.display = DisplayStyle.Flex;
         }
         else
         {
@@ -121,81 +210,109 @@ public class PlayerUIController : MonoBehaviour
         }
     }
 
+    // Methods
+    
+    #region Stats HUD
     void HpUpgrade()
     {
-        if (hpProgressBar.value < 10 && player.statPoints>0)
+        if (_hpProgressBar.value < 10 && player.statPoints>0)
         {
             Debug.Log(player.statPoints);
             Debug.Log(player.statPoints.ToString());
-            hpProgressBar.value += 1f;
+            _hpProgressBar.value += 1f;
             player.HpUpgrade();
         }
     }
     void DmgUpgrade()
     {
-        if (dmgProgressBar.value < 10 && player.statPoints>0)
+        if (_dmgProgressBar.value < 10 && player.statPoints>0)
         {
-            dmgProgressBar.value += 1f;
+            _dmgProgressBar.value += 1f;
             player.DmgUpgrade();
         }
     }
     void FirerateUpgrade()
     {
-        if (firerateProgressBar.value < 10 && player.statPoints>0)
+        if (_firerateProgressBar.value < 10 && player.statPoints>0)
         {
-            firerateProgressBar.value += 1f;
+            _firerateProgressBar.value += 1f;
             player.FirerateUpgrade();
         }
     }
     void MovespeedUpgrade()
     {
-        if (movespeedProgressBar.value < 10 && player.statPoints>0)
+        if (_movespeedProgressBar.value < 10 && player.statPoints>0)
         {
-            movespeedProgressBar.value += 1f;
+            _movespeedProgressBar.value += 1f;
             player.MovespeedUpgrade();
         }
     }
     void HpregenUpgrade()
     {
-        if (hpregenProgressBar.value < 10 && player.statPoints>0)
+        if (_hpregenProgressBar.value < 10 && player.statPoints>0)
         {
-            hpregenProgressBar.value += 1f;
+            _hpregenProgressBar.value += 1f;
             player.HpregenUpgrade();
         }
     }
-
     void HideStats()
     {
-        statsShown.style.display = DisplayStyle.None;
-        statsHidden.style.display = DisplayStyle.Flex;
+        _statsShown.style.display = DisplayStyle.None;
+        _statsHidden.style.display = DisplayStyle.Flex;
     }
-
     void ShowStats()
     {
-        statsShown.style.display = DisplayStyle.Flex;
-        statsHidden.style.display = DisplayStyle.None;
-    }
-
-    void HideHealth()
-    {
-        healthBarContainer.style.display = DisplayStyle.None;
+        _statsShown.style.display = DisplayStyle.Flex;
+        _statsHidden.style.display = DisplayStyle.None;
     }
     
+
+    #endregion
+    
+    #region Player
+    void HideHealth()
+    {
+        _healthBarContainer.style.display = DisplayStyle.None;
+    }
     void UpdatePlayerHealth()
     {
-        healthBar.highValue = player.maxHp;
-        healthBar.value = player.currentHp;
+        _healthBar.highValue = player.maxHp;
+        _healthBar.value = player.currentHp;
 
     }
+    #endregion
 
+    #region Death Screen
+    
     public void DeathScreen()
     {
-        uiScreen.style.display = DisplayStyle.None;
-        deathScreen.style.display = DisplayStyle.Flex;
+        _uiScreen.style.display = DisplayStyle.None;
+        _deathScreen.style.display = DisplayStyle.Flex;
     }
 
     void ContinueToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+    
+    #endregion
+
+    #region Esc Screen
+
+    void SaveGame()
+    {
+        
+    }
+
+    void ShowEscScreen()
+    {
+        _escScreen.style.display = DisplayStyle.Flex;
+    }
+    
+    void CloseEscScreen()
+    {
+        _escScreen.style.display = DisplayStyle.None;
+    }
+    #endregion
+    
 }
